e4dc9727763f878fea8210fb5e3d124f0f02fcd9
xumx <xumx@cpchain.io>
2018-04-19 16:09:27 +0800

    test

---
+++ tests/test_core_logic.py
@@ -0,0 +1,17 @@
+# Tests are splitted into different files, here are the APIs used.
+# from cpchain.chain.models import OrderInfo
+#
+#
+ test_trans_id = 0
+
+
+ def test_place_order():
+	print("test")
+	pass
+
+
+ def test_withdraw_order():
+	print("fdsfdsfsed")
+    pass
+
+

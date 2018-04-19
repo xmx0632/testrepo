cf9d51940972667f8875862466b01ccb574069f2
xumx <xumx@cpchain.io>
2018-04-19 18:24:43 +0800

    test

--- install-deps.sh
+++ install-deps.sh
@@ -5,6 +5,6 @@ echo "===== start install requirement.txt ====="
 pip3 install -Ur requirement.txt
 
 
-echo "===== start install requirements-dev.txt ====="
-pip3 install -Ur requirements-dev.txt
+#echo "===== start install requirements-dev.txt ====="
+#pip3 install -Ur requirements-dev.txt
 

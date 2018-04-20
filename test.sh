07047f4a3dd5f952deb5ccb08fe227683b48d73e
xmx0632 <xumx@cpchain.io>
2018-04-20 13:47:19 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 PATH=$WORKSSPACE/venv/bin:/usr/local/bin:$PATH
-EXPORT PATH
 echo "test - - test"
 
 which python3
@@ -7,10 +6,12 @@ which pip3
 which virtualenv
 
 python3 --version
-pwd
+
 if [ ! -d "venv" ]; then
-    virtualenv -p /usr/bin/python3 venv
+    pwd
 fi
+
+virtualenv -p /usr/bin/python3 venv
 echo "activate"
 
 ls $WORKSSPACE/$JOB_NAME/venv/
